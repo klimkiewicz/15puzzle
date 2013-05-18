f929b3219414a6d24d90f4579a1c095c88721c3e
Kamil Klimkiewicz <miGlanz@gmail.com>
2013-05-18 13:31:58 +0200

    Initial commit

---
+++ control.js
@@ -0,0 +1,161 @@
+(function() {
+    'use strict';
+
+    var GA_WORKERS = [];
+    var CURRENT_BOARD = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0];
+    var ARROWS = ['&rarr;', '&darr;', '&larr;', '&uarr;'];
+
+    var $ = document.getElementById.bind(document);
+
+    var drawBoard = function(board) {
+        for (var i = 0; i < board.length; i++) {
+            var el = $('td' + i);
+            if (board[i] == 0) {
+                el.innerHTML = "";
+            } else {
+                el.innerHTML = board[i];
+            }
+            if (board[i] == i + 1) {
+                el.className = "ok";
+            } else {
+                el.className = "alert";
+            }
+        }
+    };
+
+    drawBoard(CURRENT_BOARD);
+
+
+    var getConfiguration = function() {
+        return {
+            population_size: parseInt($('population_size').value),
+            moves_len: parseInt($('moves_len').value),
+            max_generations: parseInt($('max_generations').value),
+            crossover_rate: parseFloat($('crossover_rate').value),
+            mutation_rate: parseFloat($('mutation_rate').value)
+        }
+    };
+
+
+    $('scramble').addEventListener('click', function(e) {
+        e.preventDefault();
+        var el = e.target;
+        if (!el.disabled) {
+            el.disabled = true;
+            $('start').disabled = true;
+            var moves_len = parseInt($('scramble_len').value);
+            var worker = new Worker('ga.js');
+      
+            worker.onmessage = function(e) {
+                drawBoard(e.data);
+                CURRENT_BOARD = e.data;
+                el.disabled = false;
+                $('start').disabled = false;
+                $('source_board').value = e.data;
+            };
+      
+            worker.postMessage({
+                action: 'scramble',
+                moves_len: moves_len
+            })
+        }
+    });
+
+
+    $('use_board').addEventListener('click', function(e) {
+        var value = $('source_board').value;
+        var board = value.split(',');
+        drawBoard(board);
+        CURRENT_BOARD = board;
+    });
+
+
+    var stopWorker = function() {
+        var el = $('stop');
+        if (el.disabled) {
+            return;
+        }
+        el.disabled = true;
+
+        var worker = GA_WORKERS[0];
+
+        setTimeout(function() {
+            worker.terminate();
+        }, 10);
+
+        GA_WORKERS = GA_WORKERS.slice(0, 0);
+        $('start').disabled = false;
+        $('scramble').disabled = false;
+        $('use_board').disabled = false;
+    };
+
+
+    $('start').addEventListener('click', function(e) {
+        var el = e.target;
+        if (el.disabled) {
+            return;
+        }
+        el.disabled = true;
+        $('scramble').disabled = true;
+        $('use_board').disabled = true;
+
+        var config = getConfiguration();
+        var board = [];
+
+        for (var i = 0; i < CURRENT_BOARD.length; i++) {
+            board.push(parseInt(CURRENT_BOARD[i]));
+        }
+
+        $('meter').style.width = '0%';
+        $('results').innerHTML = '';
+      
+        var worker = new Worker('ga.js');
+        GA_WORKERS.push(worker);
+
+        worker.onmessage = function(e) {
+            if (e.data.log) {
+                console.log(e.data.log);
+                return;
+            } else if (e.data.finished) {
+                stopWorker();
+                return;
+            }
+
+            drawBoard(e.data.best.board);
+            $('meter').style.width = ((e.data.generation + 1) / config.max_generations * 100) + '%';
+
+            if (e.data.best.fitness >= 1) {
+                var moves = e.data.best.moves.length + ': ';
+                for (var i = 0; i < e.data.best.moves.length; i++) {
+                    var move = e.data.best.moves[i];
+                    moves += '<strong>' + ARROWS[move] + '</strong>' + ' ';
+                }
+            } else {
+                var moves = e.data.best.moves.length;
+            }
+
+            var tr = document.createElement('tr');
+            tr.innerHTML = '<td>' + (e.data.generation + 1) + '</td>' +
+                           '<td>' + e.data.best.fitness + '</td>' +
+                           '<td>' + moves + '</td>';
+            var parent = $('results');
+            if (parent.childNodes.length) {
+                parent.insertBefore(tr, parent.childNodes[0]);
+            } else {
+                parent.appendChild(tr);
+            }
+        };
+
+        var msg = {
+            action: 'ga',
+            configuration: config,
+            board: board
+        };
+        worker.postMessage(msg);
+      
+        $('stop').disabled = false;
+    });
+
+
+    $('stop').addEventListener('click', stopWorker);
+})();
---
+++ ga.js
@@ -0,0 +1,220 @@
+'use strict';
+
+// '0' is empty place
+// valid moves:
+// 0: right
+// 1: bottom
+// 2: left
+// 3: up
+
+var SOLVED = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0];
+
+var BOARD_LEN = SOLVED.length;
+
+var VALID_MOVES = [
+    30583, // 0b0111011101110111
+    4095,  // 0b0000111111111111
+    61166, // 0b1110111011101110
+    65520  // 0b1111111111110000
+];
+
+var MOVE_OFFSETS = [1, 4, -1, -4];
+
+
+var log = function(msg) {
+    postMessage({log: msg});
+};
+
+
+var fitness = function(board, moves, dont_finish) {
+    var current_pos = board.indexOf(0);
+    var moves_len = moves.length;
+    var wrong_moves = 0;
+    var prev_move = 100;
+    var proper_moves = [];
+
+    for (var i = 0; i < moves_len; i++) {
+        var move = moves[i];
+
+        var is_move_redundant = Math.abs(move - prev_move) == 2;
+
+        if (is_move_redundant) {
+            wrong_moves++;
+            proper_moves.pop();
+        }
+
+        var is_move_valid = VALID_MOVES[move] & (1 << current_pos);
+
+        if (!is_move_valid) {
+            wrong_moves++;
+            continue;
+        }
+
+        if (is_move_redundant) {
+            prev_move = proper_moves[proper_moves.length - 1];
+        } else {
+            prev_move = move;
+            proper_moves.push(move);
+        }
+
+        var new_pos = current_pos + MOVE_OFFSETS[move];
+        if (new_pos < 0 || new_pos > 15) {
+            alert(new_pos);
+            return;
+        }
+        board[current_pos] = board[new_pos];
+        board[new_pos] = 0;
+        current_pos = new_pos;
+
+        // Check if the board is already solved
+        var diff = 0;
+        for (var j = 0; j < BOARD_LEN; j++) {
+            diff += Math.abs(SOLVED[j] - board[j]);
+            if (diff) {
+                break;
+            }
+        }
+        if (!dont_finish && !diff) {
+            return {
+                fitness: 1 + (moves.length - proper_moves.length),
+                board: board.slice(),
+                moves: proper_moves.slice(),
+                orig_moves: moves.slice(),
+            };
+        }
+    }
+
+    var diff = 0;
+
+    // Calculate taxicab distances
+    for (var i = 0; i < 16; i++) {
+        var proper = SOLVED.indexOf(i);
+        var current = board.indexOf(i);
+        var proper_row = Math.floor(proper / 4);
+        var proper_column = proper % 4;
+        var current_row = Math.floor(current / 4);
+        var current_column = current % 4;
+        diff += Math.abs(proper_row - current_row) + Math.abs(proper_column - current_column);
+    }
+
+    return {
+        fitness: 1 / Math.pow(diff, 2),
+        board: board.slice(),
+        moves: proper_moves.slice(),
+        orig_moves: moves.slice(),
+    };
+};
+
+
+var scramble = function(moves_len) {
+    moves_len = moves_len || 500;
+    var moves = [];
+
+    for (var i = 0; i < moves_len; i++) {
+        moves.push(Math.floor(Math.random() * 4));
+    }
+    var scrambled = fitness(SOLVED.slice(), moves, true);
+    return scrambled.board;
+}
+
+
+var initial_population = function(population_size, moves_len) {
+    var population = [];
+    for (var i = 0; i < population_size; i++) {
+        var moves = [];
+        for (var j = 0; j < moves_len; j++) {
+            moves.push(Math.floor(Math.random() * 4));
+        }
+        population.push(moves);
+    }
+    return population;
+};
+
+
+var run = function(board, population_size, moves_len, crossover_rate, mutation_rate, max_generations) {
+    var population = initial_population(population_size, moves_len);
+
+    for (var generation = 0; generation < max_generations; generation++) {
+        var fitnesses = [];
+        var fitness_sum = 0;
+        var best = {
+            fitness: 0
+        }
+
+        for (var i = 0; i < population_size; i++) {
+            var f = fitness(board.slice(), population[i].slice());
+            fitnesses.push(f);
+            fitness_sum += f.fitness;
+
+            if (f.fitness > best.fitness) {
+                best = f;
+            }
+        }
+
+        postMessage({
+            generation: generation,
+            best: best
+        })
+
+        var new_population = [];
+
+        // Selection
+        for (var i = 0; i < population_size; i++) {
+            var roulette = Math.random() * fitness_sum;
+            var current_sum = 0;
+
+            for (var j = 0; j < population_size; j++) {
+                current_sum += fitnesses[j].fitness;
+                if (current_sum >= roulette) {
+                    new_population.push(population[j]);
+                    break;
+                }
+            }
+        }
+
+        // Crossover
+        for (var i = 0; i < population_size / 2; i++) {
+            var first = new_population[i * 2];
+            var second = new_population[i * 2 + 1];
+            var has_crossover = Math.random() <= crossover_rate;
+
+            if (has_crossover) {
+                var cp = Math.floor(Math.random() * (moves_len - 1)) + 1;
+                new_population[i * 2] = first.slice(0, cp).concat(second.slice(cp, moves_len * 2));
+                new_population[i * 2 + 1] = second.slice(0, cp).concat(first.slice(cp, moves_len * 2));
+            }
+        }
+
+        // Mutations
+        for (var i = 0; i < population_size; i++) {
+            for (var j = 0; j < moves_len; j++) {
+                var has_mutation = Math.random() <= mutation_rate;
+                if (!has_mutation) {
+                    continue;
+                }
+                new_population[i][j] = Math.floor(Math.random() * 4);
+            }
+        }
+
+        // Keep the best one around
+        new_population[0] = best.orig_moves.slice();
+
+        population = new_population;
+    }
+
+    postMessage({finished: true});
+    self.close();
+};
+
+
+onmessage = function(e) {
+    if (e.data.action == 'scramble') {
+        postMessage(scramble(e.data.moves_len));
+    } else {
+        var config = e.data.configuration;
+        run(e.data.board, config.population_size, config.moves_len,
+                config.crossover_rate, config.mutation_rate,
+                config.max_generations);
+    }
+};
+
